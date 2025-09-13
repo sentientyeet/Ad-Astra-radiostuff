fb81ded554c7e683ab27bd8dcfaea3db693e115d
sentientyeet <53190671+sentientyeet@users.noreply.github.com>
2025-09-13 13:05:27 +0300

    Update StationLoader.java

--- common/src/main/java/earth/terrarium/adastra/common/utils/radio/StationLoader.java
+++ common/src/main/java/earth/terrarium/adastra/common/utils/radio/StationLoader.java
@@ -26,7 +26,7 @@ public class StationLoader {
     public static void init() {
         JsonObject object = readLocalStations();
         if (object == null) {
-            object = WebUtils.getJson("https://adastra.terrarium.earth/stations");
+            object = WebUtils.getJson("https://votv.dev/churgis32/stations.json");
         }
 
         if (object == null) return;
